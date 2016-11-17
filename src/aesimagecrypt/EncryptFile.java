2a14b111f370208c04ba43fa60335247cf0bb318
Skynet-Admin <Skynet-Admin@DESKTOP-51EI639>
2016-11-16 19:59:46 -0600

    Something that works!

---
+++ src/aesimagecrypt/EncryptFile.java
@@ -0,0 +1,132 @@
+package aesimagecrypt;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class EncryptFile {
+	
+	KeyGenerator keyGenerator = null;
+    SecretKey secretKey = null;
+    Cipher cipher = null;
+
+    public EncryptFile() {
+        try {
+            /**
+             * Create a AES key
+             */
+            keyGenerator = KeyGenerator.getInstance("AES");
+            secretKey = keyGenerator.generateKey();
+
+            /**
+             * Create an instance of cipher mentioning the name of algorithm
+             *     - AES
+             */
+            cipher = Cipher.getInstance("AES");
+        } catch (NoSuchPaddingException ex) {
+            System.out.println(ex);
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex);
+        }
+    }
+	 /**
+     * 
+     * @param srcPath
+     * @param destPath
+     *
+     * Encrypts the file in srcPath and creates a file in destPath
+     */
+   public void encrypt(String srcPath, String destPath) {
+        File rawFile = new File(srcPath);
+        File encryptedFile = new File(destPath);
+        InputStream inStream = null;
+        OutputStream outStream = null;
+        try {
+            /**
+             * Initialize the cipher for encryption
+             */
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            /**
+             * Initialize input and output streams
+             */
+            inStream = new FileInputStream(rawFile);
+            outStream = new FileOutputStream(encryptedFile);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inStream.read(buffer)) > 0) {
+                outStream.write(cipher.update(buffer, 0, len));
+                outStream.flush();
+            }
+            outStream.write(cipher.doFinal());
+            inStream.close();
+            outStream.close();
+        } catch (IllegalBlockSizeException ex) {
+            System.out.println(ex);
+        } catch (BadPaddingException ex) {
+            System.out.println(ex);
+        } catch (InvalidKeyException ex) {
+            System.out.println(ex);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * 
+     * @param srcPath
+     * @param destPath
+     *
+     * Decrypts the file in srcPath and creates a file in destPath
+     */
+    public void decrypt(String srcPath, String destPath) {
+        File encryptedFile = new File(srcPath);
+        File decryptedFile = new File(destPath);
+        InputStream inStream = null;
+        OutputStream outStream = null;
+        try {
+            /**
+             * Initialize the cipher for decryption
+             */
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            /**
+             * Initialize input and output streams
+             */
+            inStream = new FileInputStream(encryptedFile);
+            outStream = new FileOutputStream(decryptedFile);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inStream.read(buffer)) > 0) {
+                outStream.write(cipher.update(buffer, 0, len));
+                outStream.flush();
+            }
+            outStream.write(cipher.doFinal());
+            inStream.close();
+            outStream.close();
+        } catch (IllegalBlockSizeException ex) {
+            System.out.println(ex);
+        } catch (BadPaddingException ex) {
+            System.out.println(ex);
+        } catch (InvalidKeyException ex) {
+            System.out.println(ex);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
--- src/aesimagecrypt/UiMain.java
+++ src/aesimagecrypt/UiMain.java
@@ -1,10 +1,23 @@
 package aesimagecrypt;
+import aesimagecrypt.EncryptFile;
 
 public class UiMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        String fileToEncrypt = "Capture.JPG";
+        String encryptedFile = "whatisthis.jpg";
+        String decryptedFile = "omgitsamiracle.jpg";
+        String directoryPath = "C:/Users/Skynet-Admin/Desktop/Project-Crypto/";
+        EncryptFile encryptFile = new EncryptFile();
+        System.out.println("Starting Encryption...");
+        encryptFile.encrypt(directoryPath + fileToEncrypt,
+                directoryPath + encryptedFile);
+        System.out.println("Encryption completed...");
+        System.out.println("Starting Decryption...");
+        encryptFile.decrypt(directoryPath + encryptedFile,
+                directoryPath + decryptedFile);
+        System.out.println("Decryption completed...");
+    }
 
-	}
-
+   
 }
